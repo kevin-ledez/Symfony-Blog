2cd034e884fa4a3c1a52a8810a24c8b03975a259
kevin-ledez <kevin.ledez@gmail.com>
2023-08-20 22:05:48 +0200

    📦 NEW: Widjet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
 import '../css/app.scss';
+import { Dropdown } from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+    enableDropdowns();
+});
+
+const enableDropdowns = () => {
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+    dropdownElementList.map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+};
